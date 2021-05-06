528195c60913e957625bad4186d5052c28c25c64
Katerina Frid <catherinefrid96@gmail.com>
2021-05-06 08:09:26 +0300

    change model path

--- backend/app/utils/inference.py
+++ backend/app/utils/inference.py
@@ -20,7 +20,7 @@ logger.addHandler(ch)
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = 'cpu'
 logger.info(os.getcwd(os.listdir())
-logger.info(os.listdir('/usr/src/app/server')
+logger.info(os.listdir('/usr/src/app/server'))
 tokenizer = GPT2Tokenizer.from_pretrained('/usr/src/app/server/models/gpt2')
 model = GPT2LMHeadModel.from_pretrained('/usr/src/app/server/models/gpt2').eval()
 model = model.to(device)
